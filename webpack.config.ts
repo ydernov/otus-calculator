6030d0dd30ac27cec479436a54fb61fff7fa2006
Ярослав Дернов <yaroslav.dernov@russianpost.ru>
2021-10-06 21:06:16 +0300

    Initial project setup: - Added webpack + config - Added typescript + config - Added initial packages

---
+++ webpack.config.ts
@@ -0,0 +1,27 @@
+import * as path from 'path';
+import * as webpack from 'webpack';
+// in case you run into any typescript error when configuring `devServer`
+import 'webpack-dev-server';
+
+const config: webpack.Configuration = {
+  // mode: 'production',
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'calc_bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
+
+export default config;
